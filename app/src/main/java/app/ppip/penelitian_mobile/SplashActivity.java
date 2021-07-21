df154c57b684f060fbfe4669a9ab6b10737e99cb
taufan26 <taufanyusuf25@gmail.com>
2021-07-21 22:33:25 +0700

    splash screen

---
+++ app/src/main/java/app/ppip/penelitian_mobile/SplashActivity.java
@@ -0,0 +1,51 @@
+package app.ppip.penelitian_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Space;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN = 5000;
+
+    Animation topAnim, bottomAnim;
+    TextView nama, mobile;
+    ImageView image;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        nama = findViewById(R.id.polindra);
+        mobile = findViewById(R.id.mobileapp);
+
+        image.setAnimation(topAnim);
+        nama.setAnimation(bottomAnim);
+        mobile.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
